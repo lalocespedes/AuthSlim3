d2f9b001a228faa2e62158b0f72a76a32571e08b
Eduardo Cespedes <lalocespedes@gmail.com>
2016-11-13 22:11:46 +0000

    changes

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
+$dotenv->required([
+    'DB_CONNECTION',
+    'DB_SERVER',
+    'DB_DATABASE',
+    'DB_USER',
+    'DB_PASSWORD'
+]);
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
+$app->run();
---
+++ src/routes.php
@@ -0,0 +1,24 @@
+<?php
+// Routes
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // $this->db->table('users')->insert([
+    //     'name' => 'lalocespedes',
+    //     'email' => 'lalocespedes@gmail.com',
+    //     'password' => '123'
+    //     ]);
+
+    // $this->db->table('users')->where('id', 2)->delete();
+
+    $users = $this->db->table('users')->get();
+
+    dump($users);
+
+    exit;
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
